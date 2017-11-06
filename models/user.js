f6b956fdd9e9ae7f9daa06d5324b71642444a070
eyerean <eptinea@gmail.com>
2017-11-06 16:14:41 +0100

    encrypt password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 //Define the user model
 const userSchema = new Schema({
   email: {type: String, unique: true, lowercase: true},
   password: String
+});
+
+//On save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) throw err;
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) throw err;
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 //Create the model classe
